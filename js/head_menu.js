14ad890666f623768036eabbfcb0b82360bad2cb
anythingelse22 <48355032+anythingelse22@users.noreply.github.com>
2022-07-26 00:50:59 +0300

    Add files via upload

--- js/head_menu.js
+++ js/head_menu.js
@@ -1,16 +1,64 @@
-let menu_opened = false
+
+let page_load = true
+const dbg = false
+
+menu_click()
 function menu_click() {
 	const btn = document.querySelector(".head_menu")
 	const navbar = document.querySelector(".navmenu")
-	
-	if (!menu_opened) {
-		btn.classList.toggle("active")
-		navbar.classList.toggle("active")
-		menu_opened = true
-	} else {
-		btn.classList.remove("active")
-		navbar.classList.toggle("active")
-		menu_opened = false
+	let menu = sessionStorage.getItem("menu")
+	pr("menu is: %s || type: ", menu, typeof(menu))
+
+	if (menu === null || menu === undefined) {
+		sessionStorage.setItem("menu", "false")
+		menu = sessionStorage.getItem("menu")
 	}
 
+	// page loaded
+	if (page_load) {
+		pr("page_load")
+		page_load = false
+		// and menu_open is true
+		if (menu === "true") {
+			// Show Menu
+			toggle_menu(btn, navbar)
+			pr("page loaded and menu is %s: Show Menu", menu)
+		// and menu_open is false or null
+		} else if (menu === "false") {
+			// Do not Show menu
+			pr("page loaded, but menu is %s: Do not Show Menu", menu)
+		}
+	// click callback
+	} else {
+		pr("click")
+		// menu_open is true
+		if (menu === "true") {
+			// Close menu
+			toggle_menu(btn, navbar, "false")
+			pr("click, menu is true: Close Menu")
+		} else if (menu === "false") {
+			// Show menu
+			toggle_menu(btn, navbar, "true")
+			pr("click, menu is false: Show Menu")
+		}
+	}
+
+}
+
+function toggle_menu(obj1, obj2, val) {
+	obj1.classList.toggle("active")
+	obj2.classList.toggle("active")
+
+	if (val === "true" || val === "false") {	// fkn js, dont understand the null eval xd
+		sessionStorage.setItem("menu", val)
+	}
+}
+
+function remove_menu_item() {
+	sessionStorage.removeItem("menu")
+}
+
+function pr(...otherInfo) {
+	if (!dbg) { return }
+	console.log(...otherInfo)
 }
